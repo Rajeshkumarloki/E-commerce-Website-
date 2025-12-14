ce252a5c8f076271bde87f7a2ebd2af11015336b
Rajesh Kumar V <rajeshkumar2004r21@gmail.com>
2025-12-14 10:45:58 +0530

    Update AllProducts.jsx

--- src/AllProducts.jsx
+++ src/AllProducts.jsx
@@ -1,34 +1,3 @@
-// import React from "react";
-// import "./AllProducts.css";
-
-// const AllProducts = (props) => {
-//     console.log(props);
-//     let { productState } = props;
-
-//     console.log(productState);
-
-//     return
-//     (
-//         <main>
-//             {
-//                 productState.map((e, i) => {
-//                     return (
-//                         <div key={i}>
-//                             <img src={e.image} alt="" />
-//                             <h2>{e.title}</h2>
-//                             <h3>Price :Rs.{e.price}</h3>
-//                             <h4>Rating : {e.rating.rate}</h4>
-//                             <button>Add To Cart</button>
-//                         </div>
-//                     )
-//                 })
-//             }
-//         </main>
-//     );
-// }
-
-// export default AllProducts;
-
 import React from "react";
 import "./AllProducts.css";
 
